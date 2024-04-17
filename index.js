ccc04d9043fab340a39d679972161c1e39d70d32
Sven <Sven.Laht@gmail.com>
2024-04-17 11:43:38 +0300

    aaaaaa

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import express from 'express';
-import bodyParser from 'pody-parser';
+import nunjucks from 'nunjucks';
+import bodyParser from 'body-parser';
+const app = express();
+const port = 3000;
+const __dirname = import.meta.dirname;
 
-let app = express()
-let port = 3000
+app.use(bodyParser.urlencoded({ extended: false }));
 
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+// req => request , res => response
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    console.log(req.query);
+    res.render('index.njk');
+});
+
+app.get('/answer', (req, res) => {
+    console.log(req.query);
+    res.render('answer.njk', req.query);
+    
+});
+app.post('/answer', (req, res) => {
+    console.log(req.body);
+    res.render('answer.njk', {...req.body,...req.query});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
